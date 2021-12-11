0923dfc46ff536a5252685f0af8cd2affba84b2b
vivek raghav <you@example.com>
2021-12-11 13:38:21 +0530

    Ecommerce

---
+++ frontend/src/component/layout/About/About.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./aboutSection.css";
+import { Button, Typography, Avatar } from "@material-ui/core";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import InstagramIcon from "@material-ui/icons/Instagram";
+const About = () => {
+  const visitInstagram = () => {
+    window.location = "https://www.instagram.com/vivek_singh_raghav/";
+  };
+  return (
+    <div className="aboutSection">
+      <div></div>
+      <div className="aboutSectionGradient"></div>
+      <div className="aboutSectionContainer">
+        <Typography component="h1">About Us</Typography>
+
+        <div>
+          <div>
+            <Avatar
+              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
+              src="https://res.cloudinary.com/rgv23/image/upload/q_100/v1637325363/avatars/ewuvlr0u8kc8gi4dlv9u.jpg"
+              alt="Founder"
+            />
+            <Typography>Vijay Singh Raghav</Typography>
+            <Button onClick={visitInstagram} color="primary">
+              Visit Instagram
+            </Button>
+            <span>
+              This is a sample wesbite made by @mevijaysingh. Only with the
+              purpose to learn MERN Stack .
+            </span>
+          </div>
+          <div className="aboutSectionContainer2">
+            <Typography component="h2">Our Brands</Typography>
+            <a
+              href="https://www.youtube.com/"
+              target="blank"
+            >
+              <YouTubeIcon className="youtubeSvgIcon" />
+            </a>
+
+            <a href="https://www.instagram.com/vivek_singh_raghav/" target="blank">
+              <InstagramIcon className="instagramSvgIcon" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
